a6ac7ad97b95460deb861c0f89e82e1dfba95d64
MonyAmani <supermaarek11@gmail.com>
2026-09-09 09:01:05 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "ordry-shell-v5";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icons/icon-180.png", "./icons/icon-192.png", "./icons/icon-512.png"];
+const CACHE = "ordry-shell-v6";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(
